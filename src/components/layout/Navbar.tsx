cb059304a940af5eb2ea3af26228e28dc1c5a6ad
devian <devian.agency@gmail.com>
2025-11-08 02:19:24 +0530

    Added link to website logo

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -15,9 +15,11 @@ export default function Navigation(){
   return (
     <header>
       <nav className="h-12 w-full flex justify-between items-center gap-4 shadow-soft border border-white px-4">
-        <div className="w-1/4 flex items-center gap-2">
-        <ImageWrapper src={"/logo/logo-transparent.png"} alt="Challenge's Logo" className="size-8" />
-          <h1 className="font-instrument-sans font-medium text-2xl">Challenge</h1>
+        <div className="w-1/4">
+        <Link href={"/"} className="flex items-center gap-2 w-fit">
+          <ImageWrapper src={"/logo/logo-transparent.png"} alt="Challenge's Logo" className="size-8" />
+            <h1 className="font-instrument-sans font-medium text-2xl">Challenge</h1>
+        </Link>
         </div>
         <div className="w-1/2 h-full hidden md:block">
           <input type="search" onInput={(e: React.ChangeEvent<HTMLInputElement>)=> setSearch(e.target.value)} onChange={(e) => setSearch(e.target.value)} value={search} className="w-full h-full px-4 font-instrument-sans text-xl focus-within:outline-none " placeholder="Search your content here..." />
