5c411afdd51809c88896e3981e765a309194402f
devian <devian.agency@gmail.com>
2025-11-07 23:32:44 +0530

    Day 5 Challenge - Twitter/X Animated SVG Icon

--- src/app/challenge/series/60-days/day-5/page.tsx
+++ src/app/challenge/series/60-days/day-5/page.tsx
@@ -38,7 +38,7 @@ function Day2() {
       <div
         className="size-fit mx-auto bg-icon p-5 rounded-full cursor-pointer"
       >
-        <TwitterXIcon />
+        <Twitter />
       </div>
     </div>
   );
@@ -54,7 +54,7 @@ interface IconProps {
 const twitter = "M22.46 6c-.77.35-1.6.58-2.47.69a4.26 4.26 0 0 0 1.87-2.35 8.48 8.48 0 0 1-2.7 1.03 4.24 4.24 0 0 0-7.23 3.87 12.03 12.03 0 0 1-8.74-4.43 4.22 4.22 0 0 0 1.31 5.66 4.18 4.18 0 0 1-1.92-.53v.05c0 2.03 1.45 3.72 3.37 4.1a4.26 4.26 0 0 1-1.91.07 4.25 4.25 0 0 0 3.96 2.95A8.5 8.5 0 0 1 2 19.54a12 12 0 0 0 6.5 1.9c7.8 0 12.07-6.46 12.07-12.07l-.01-.55A8.64 8.64 0 0 0 22.46 6z";
 const x = "M14.283 10.18L23.318 0H21.26l-7.82 8.441L7.296 0H0l9.363 13.597L0 24h2.047l8.241-9.867L16.99 24h7.01l-9.717-13.82ZM11.24 13.652L10.4 12.458 2.798 1.563h3.286l6.26 9.306.839 1.194 7.76 11.217h-3.286l-6.417-9.628Z";
 
-function TwitterXIcon({
+function Twitter({
   size = 64,
   duration = 0.5,
   hold = 2,
@@ -116,7 +116,7 @@ function TwitterXIcon({
       height={size}
       role="img"
       fill="currentColor"
-      aria-label="Twitter to X animated icon"
+      aria-label="Twitter to X animated svg icon"
     >
       <path ref={pathRef} fill="currentColor" d={twitter} />
     </svg>
