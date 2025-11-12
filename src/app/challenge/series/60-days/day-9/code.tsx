7e99e4cb65b9c671622963b11506cf1c3bd9c957
devian <devian.agency@gmail.com>
2025-11-12 13:35:53 +0530

    Day 9 Challenge - Image Card - responsiveness fixed

--- src/app/challenge/series/60-days/day-9/code.tsx
+++ src/app/challenge/series/60-days/day-9/code.tsx
@@ -29,14 +29,14 @@ export default function Day5Page() {
 
 export function Day9() {
   return (
-    <section className="max-w-7xl mx-auto h-full flex items-center gap-4">
+    <section className="max-w-7xl mx-auto h-full flex flex-wrap items-center gap-4">
           <Card heading="Smart AI focus" image="/images/challenge/series/60-days/day-9/image1.avif">
             Devian shows what to focus on — based on your goals and pace.
           </Card>
           <Card heading="Track your progress" image="/images/challenge/series/60-days/day-9/image2.avif">
             Track what’s done, what’s next — all in one dashboard.
           </Card>
-          <Card className="h-92" heading="Never miss tasks" image="/images/challenge/series/60-days/day-9/image3.avif">
+          <Card className="" heading="Never miss tasks" image="/images/challenge/series/60-days/day-9/image3.avif">
           Calendar view of upcoming tasks, so you never miss a thing.
           </Card>
         </section>
